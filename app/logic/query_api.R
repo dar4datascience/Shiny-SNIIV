e02fd595603c9a4b9d71b90afa3c52221d6cc948
dar4ds <amievarodriguez.daniel@gmail.com>
2024-01-06 21:32:54 -0600

    update read me

--- app/logic/query_api.R
+++ app/logic/query_api.R
@@ -1,15 +1,15 @@
-url="https://sniiv.sedatu.gob.mx/api/CuboAPI/GetFinanciamiento/1973,2023/0/0/anio,organismo,modalidad"
-
-
-library(httr)
-library(jsonlite)
-library(dplyr)
-
-
-#Ejectuar la petición
-r <- GET(url)
-
-df <- fromJSON(rawToChar(r$content)) |> 
-  arrange(año)
-
-df
+# url="https://sniiv.sedatu.gob.mx/api/CuboAPI/GetFinanciamiento/1973,2023/0/0/anio,organismo,modalidad"
+# 
+# 
+# library(httr)
+# library(jsonlite)
+# library(dplyr)
+# 
+# 
+# #Ejectuar la petición
+# r <- GET(url)
+# 
+# df <- fromJSON(rawToChar(r$content)) |> 
+#   arrange(año)
+# 
+# df
